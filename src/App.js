e624a7050cdc646ead0f749b7e0708ccd1e64618
Felipe Corso <felipe.corso@dunelm.com>
2023-03-29 18:04:47 +0100

    Products page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Products from "./pages/products";
 
 export default function App() {
-  return <div>This is the</div>;
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            <Route path="product" element={<Products />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
